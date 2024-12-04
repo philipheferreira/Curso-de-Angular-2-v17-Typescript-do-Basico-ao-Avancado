e82ea53a7be7afb3e47cf078621d8cd03283927a
philipheferreira <philipheferreira@gmail.com>
2024-12-04 17:23:15 -0300

    Feat: components change

--- Aulas/4 - Components/3 - Criando um componente/src/app/components/new-component/new-component.component.ts
+++ Aulas/4 - Components/3 - Criando um componente/src/app/components/new-component/new-component.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
-
+import { TesteComponent } from '../teste/teste.component';
 @Component({
   selector: 'app-new-component',
-  imports: [],
+  imports: [TesteComponent],
   templateUrl: './new-component.component.html',
   styleUrl: './new-component.component.scss'
 })
