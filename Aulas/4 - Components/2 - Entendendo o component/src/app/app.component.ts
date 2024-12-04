326bf72763f7160ad657e7604a22d08054abb2b2
philipheferreira <philipheferreira@gmail.com>
2024-12-04 14:24:28 -0300

    Feat: adjust aplication

--- Aulas/4 - Components/2 - Entendendo o component/src/app/app.component.ts
+++ Aulas/4 - Components/2 - Entendendo o component/src/app/app.component.ts
@@ -1,11 +1,19 @@
+/* Para esse projeto será deletado os arquivos components de html, css e spec e sera utilizado
+apenas o que tiver dentro do component */
+
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [RouterOutlet], // caso precise adicionar uma dependencia é só digitar no imports que será adicionado automaticamente
+  template: `
+    <h1>Component angular</h1>
+    <p>Teste de texto aleatorio</p>
+  `
+  //templateUrl: './app.component.html', esse arquivo puxa um html pra focar como principal
+  //styleUrl: './app.component.css', // esse syle pode ser utilizado mas só da pra puxar 1 arquivo css
+  //styleUrls: ['./app.component.css'] // nesse podesse adicionar mais de um css
 })
 export class AppComponent {
   title = 'componentProject';
