08135d0414fa633a055892a66026849280fd7bfc
philipheferreira <philipheferreira@gmail.com>
2024-12-04 15:39:03 -0300

    FEat: component project finish

--- Aulas/4 - Components/3 - Criando um componente/src/app/app.component.ts
+++ Aulas/4 - Components/3 - Criando um componente/src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NewComponent } from './components/new-component/new-component.component'; // precisa criar pra importar um novo component
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NewComponent], // precisa declarar ele no imports
   template: `
   <h1>Template geral</h1>
   <p class="texto">Teste de texto funcional</p>
+  Antigo: <app-new-component></app-new-component> <!-- Para aparecer ele é necessario realizar a chamada -->
+  Novo: <app-new-component/>
   `,
   styles: [`
       .texto{color: blue}
@@ -15,3 +18,6 @@ import { RouterOutlet } from '@angular/router';
 export class AppComponent {
   title = 'componentProject';
 }
+
+
+// para gerar um novo componente e uma nova pasta é só digitar o comando: ng generate component components/new-component
