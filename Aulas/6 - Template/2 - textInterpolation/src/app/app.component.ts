4515d599916bd6f5d78f543f1e1d60593a10a25c
philipheferreira <philipheferreira@gmail.com>
2024-12-05 15:33:57 -0300

    Feat: text interpolation

--- Aulas/6 - Template/2 - textInterpolation/src/app/app.component.ts
+++ Aulas/6 - Template/2 - textInterpolation/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TemplateBindingComponent } from './components/template/template-binding/template-binding.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TemplateBindingComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ Aulas/6 - Template/2 - textInterpolation/src/app/components/template/template-binding/template-binding.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-template-binding',
+  imports: [],
+  templateUrl: './template-binding.component.html',
+  styleUrl: './template-binding.component.scss'
+})
+export class TemplateBindingComponent {
+
+}
