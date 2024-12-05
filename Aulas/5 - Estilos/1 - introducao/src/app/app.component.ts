a321afcc292be9c580b1d2a7862496e9606dfd7d
philipheferreira <philipheferreira@gmail.com>
2024-12-05 12:54:09 -0300

    Feat: style cap

--- Aulas/5 - Estilos/1 - introducao/src/app/app.component.ts
+++ Aulas/5 - Estilos/1 - introducao/src/app/app.component.ts
@@ -6,7 +6,15 @@ import { AngulaSvgComponent } from './components/angula-svg/angula-svg.component
   selector: 'app-root',
   imports: [RouterOutlet, AngulaSvgComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  styleUrls: ['./app.component.scss', './components/angula-svg'], // com esse comando eu consigo adicionar mais de um css em um componente
+  styles: [`
+
+      h2 {
+        color: red;
+      }
+
+    `]
 })
 export class AppComponent {
   title = '1 - introdução estilos';
