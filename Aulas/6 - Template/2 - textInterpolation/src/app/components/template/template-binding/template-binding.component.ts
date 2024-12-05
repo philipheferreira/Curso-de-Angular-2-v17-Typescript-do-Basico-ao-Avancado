3a84cfebdf823c91873d8f3980a60a5f794ee7de
philipheferreira <philipheferreira@gmail.com>
2024-12-05 15:51:01 -0300

    Feat: textInterpolation finish

--- Aulas/6 - Template/2 - textInterpolation/src/app/components/template/template-binding/template-binding.component.ts
+++ Aulas/6 - Template/2 - textInterpolation/src/app/components/template/template-binding/template-binding.component.ts
@@ -7,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrl: './template-binding.component.scss'
 })
 export class TemplateBindingComponent {
+  public nome = 'Philiphe Siqueira Ferreira';
+  public idade = 28;
 
+  public sum(valor1: number, valor2: number){
+    return valor1 + valor2;
+  }
 }
