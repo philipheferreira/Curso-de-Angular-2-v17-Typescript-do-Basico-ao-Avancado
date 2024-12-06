e318f64a2fe7a427a9d28f6610f634c0f43425bb
philipheferreira <philipheferreira@gmail.com>
2024-12-06 12:29:38 -0300

    Create: components 4 project

--- Aulas/6 - Template/3 - Property Binding/src/app/components/teste/teste.component.ts
+++ Aulas/6 - Template/3 - Property Binding/src/app/components/teste/teste.component.ts
@@ -7,7 +7,15 @@ import { Component } from '@angular/core';
   styleUrl: './teste.component.scss'
 })
 export class TesteComponent {
-  nome = 'Philiphe siqueira Ferreira';
-  idade = 28;
-  profissao = 'Engenheiro da computação'
+  public nome = 'Philiphe Siqueira Ferreira'
+  public isDisable1 = true; // Como passar um valor verdadeiro então o botão realmente é desabilitado
+  public isDisable2 = false;
+  public imgValue = 'https://media.gq.com/photos/55828b3f1177d66d68d5287c/1:1/w_450,h_450,c_limit/blogs-the-feed-how-i-met-your-mother-barney-stinson.jpg'
+
+  constructor(){
+    setTimeout( () => {
+      this.nome = "João e Jherson"
+    }, 1000);
+  }
+
 }
