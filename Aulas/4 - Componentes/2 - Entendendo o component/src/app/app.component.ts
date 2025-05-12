d22b8d6b6b4347a5b0530ff6cf2032786e5351eb
philipheferreira <philipheferreira@gmail.com>
2025-05-12 11:27:32 -0300

    Feat: module 4 cap 2 comments corrections and title variable

--- Aulas/4 - Componentes/2 - Entendendo o component/src/app/app.component.ts
+++ Aulas/4 - Componentes/2 - Entendendo o component/src/app/app.component.ts
@@ -1,21 +1,25 @@
 /* Para esse projeto será deletado os arquivos components de html, css e spec e sera utilizado
 apenas o que tiver dentro do component */
 
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
-@Component({
+@Component({ // é um decoretor. O decoretaro é um arroba com alguma coisa
   selector: 'app-root',
-  imports: [RouterOutlet], // caso precise adicionar uma dependencia é só digitar no imports que será adicionado automaticamente
+  standalone: true,
+  imports: [CommonModule ,RouterOutlet], // caso precise adicionar uma dependencia é só digitar no imports que será adicionado automaticamente
   template: `
     <h1>Component angular</h1>
     <p>Teste de texto aleatorio</p>
+
+    {{ title }}
   `,// O arquivo template é usado para renderizar template que é direcionado a ele e aparece direto na pagina
   //templateUrl: './app.component.html', esse arquivo puxa um html pra focar como principal
-  //styleUrl: './app.component.css', // esse syle pode ser utilizado mas só da pra puxar 1 arquivo css
-  //styleUrls: ['./app.component.css'] // nesse podesse adicionar mais de um css
-  styles: [`.name{}`]
+  //styleUrl: './app.component.css', // esse style pode ser utilizado mas só da pra puxar 1 arquivo css
+  //styleUrls: ['./app.component.css', '/app.componentOutro.css'] // nesse podesse adicionar mais de um css
+  styles: [`.name{}`] //A tag styles permiti programar css nesse component
 })
 export class AppComponent {
-  title = 'componentProject';
+  title = 'Projeto 2 - entendendo componentes';
 }
