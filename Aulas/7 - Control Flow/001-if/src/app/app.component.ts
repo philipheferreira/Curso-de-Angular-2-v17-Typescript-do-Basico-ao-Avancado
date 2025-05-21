b2615decb3103b1abdb66232871ed824dcd8c0bb
philipheferreira <philipheferreira@gmail.com>
2025-05-21 14:19:46 -0300

    Feat: component integration

--- Aulas/7 - Control Flow/001-if/src/app/app.component.ts
+++ Aulas/7 - Control Flow/001-if/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TemplateControlFlowComponent } from './components/template/template-control-flow/template-control-flow.component'
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TemplateControlFlowComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
