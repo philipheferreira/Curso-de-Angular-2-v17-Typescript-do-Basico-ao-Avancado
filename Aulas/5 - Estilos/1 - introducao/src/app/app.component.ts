0f16a044ed0cebcdd97a3c13b3c18ba67b5435e0
philipheferreira <philipheferreira@gmail.com>
2025-05-21 12:40:41 -0300

    Feat: create module 7

--- Aulas/5 - Estilos/1 - introducao/src/app/app.component.ts
+++ Aulas/5 - Estilos/1 - introducao/src/app/app.component.ts
@@ -6,7 +6,7 @@ import { AngulaSvgComponent } from './components/angula-svg/angula-svg.component
   selector: 'app-root',
   imports: [RouterOutlet, AngulaSvgComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+  styleUrl: './app.component.scss', // Arquivo style que é importado e usado
   //styleUrls: ['./app.component.scss', './components/angula-svg'], // com esse comando eu consigo adicionar mais de um css em um componente
   styles: [`
 
@@ -14,7 +14,7 @@ import { AngulaSvgComponent } from './components/angula-svg/angula-svg.component
         color: red;
       }
 
-    `]
+    `] // Essa Tag torna essa parte do codigo usavel para css
 })
 export class AppComponent {
   title = '1 - introdução estilos';
