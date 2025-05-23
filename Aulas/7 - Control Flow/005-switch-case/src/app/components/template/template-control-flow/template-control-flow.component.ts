484b8c885505dbbff106289c9473ac7fe8f0fe11
philipheferreira <philipheferreira@gmail.com>
2025-05-22 21:26:59 -0300

    Feat: module 7 cap  create component

---
+++ Aulas/7 - Control Flow/005-switch-case/src/app/components/template/template-control-flow/template-control-flow.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-template-control-flow',
+  imports: [],
+  templateUrl: './template-control-flow.component.html',
+  styleUrl: './template-control-flow.component.css'
+})
+export class TemplateControlFlowComponent {
+
+}
