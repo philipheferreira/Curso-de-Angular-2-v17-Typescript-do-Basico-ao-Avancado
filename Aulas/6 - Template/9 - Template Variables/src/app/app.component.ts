fdc39fd29ce80d8e0004e196987891df67704c19
philipheferreira <philipheferreira@gmail.com>
2025-06-03 15:43:51 -0300

    Feat: module 6 cap 9 create component and call

--- Aulas/6 - Template/9 - Template Variables/src/app/app.component.ts
+++ Aulas/6 - Template/9 - Template Variables/src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TemplateVariables } from './components/template/template-variables/template-variables.component'
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    TemplateVariables
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ Aulas/6 - Template/9 - Template Variables/src/app/components/template/template-variables/template-variables.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-template-variables',
+  imports: [],
+  templateUrl: './template-variables.component.html',
+  styleUrl: './template-variables.component.scss'
+})
+export class TemplateVariables { // precisa ser o mesmo nome dentro do export para ser chamado pelo outro component
+
+}
