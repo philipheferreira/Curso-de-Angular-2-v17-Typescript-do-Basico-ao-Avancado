344261dd679671e621908d15bb5e43ec27934575
philipheferreira <philipheferreira@gmail.com>
2025-06-03 13:26:08 -0300

    Feat: create basic template module 6 cap 7

--- Aulas/6 - Template/8 - NgClass e NgStyle/src/app/app.component.ts
+++ Aulas/6 - Template/8 - NgClass e NgStyle/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms'; // importar o ngModule do Forms
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule], // toda vez que for utilizar o ngModule com import necessario importar FormsModule
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'project-NgClass-NgStyle';
+  public nome = 'Philiphe Siqueira Ferreira'
 }
